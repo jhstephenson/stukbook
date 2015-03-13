b36c28c45684256983d51b46a3e79f48b7a6b6a6
James Stephenson <jstephen@scc-i.com>
2015-03-13 15:18:51 -0600

    Add friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+	def action_buttons(user)
+		case current_user.friendship_status(user) when "friends"
+			"Remove"
+		when "pending"
+			"Cancel Request"
+		when "requested"
+			"Accept"
+		when "not_friends"
+			"Add as a Friend"
+		end
+	end
+
 end
